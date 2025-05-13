2210611b19ce54f4e27c50a2ca54183eabfc49f4
Kaela Almendras <kaelaadriennealmendras@gmail.com>
2025-05-13 22:01:44 +0800

    feat: :sparkles: /api/chat integrated

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_input = data.get("message", "").lower()
+
+    if user_input == "hi":
+        return jsonify({"response": "Hello"})
+    else:
+        return jsonify({"response": "Sorry, I don't understand."})
 
 if __name__ == '__main__':
     app.run(debug=True)
